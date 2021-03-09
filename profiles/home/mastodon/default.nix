055733b26108e4a1f201f8de8332eaa58680b4a2
Hedtke, Moritz <Moritz.Hedtke@t-online.de>
2021-03-09 19:24:16 +0100

    jo

--- profiles/home/mastodon/default.nix
+++ profiles/home/mastodon/default.nix
@@ -20,4 +20,17 @@
     # TODO FIXME
     # no default user known yet
   };
+
+  # su - mastodon -s /bin/sh
+  # mastodon-env tootctl accounts create moritz_hedtke --email=EMAIL
+  # mastodon-env tootctl accounts modify moritz_hedtke --confirm
+  # mastodon-env tootctl accounts modify moritz_hedtke --role admin
+
+  # openssl s_client -connect video.pi.example.org:https
+  # openssl s_client -connect self-signed.badssl.com:https
+  # curl https://video.pi.example.org
+
+  # cd pkgs/servers/mastodon/
+  # nix build --impure --expr '(builtins.getFlake "nixpkgs").legacyPackages.x86_64-linux.callPackage ./update.nix {}'
+  # ./result/bin/update.sh --url https://github.com/mohe2015/mastodon.git --rev 4923aaa5856c822d41516e7c3d88730569853618 --ver 3.3.0
 }
